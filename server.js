c6a3d2050f738e88f244d8e50881970f2a552d08
Reshma Midhun <reshmaparasuraman@gmail.comget config --global user.name>
2025-07-22 14:37:35 +0400

    Fix CORS and PORT for Render

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post("https://ecomm-ab86.onrender.com/stripe-checkout", async(req, res) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
